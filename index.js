b7dbfdc05f8f10051b10fc9a5440bef9166a6fbe
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 19:52:18 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,43 @@
+let store = { drivers: [], passengers: [], trips: [] }
+class Person{
+  constructor(name){
+    this.name = name
+    this.id = store[this.constructor.name.toLowerCase() + "s"].push(this) - 1
+  }
+  trips(){
+    return store.trips.filter( ( trip ) => { return trip[this.constructor.name.toLowerCase()]() === this  } )
+  }
+}
+class Driver extends Person{
+    constructor(name){
+      super(name)
+    }
+   passengers(){
+    return  this.trips().map( (trip) => { return trip.passenger() } )
+   }
+}
+
+class Passenger extends Person{
+  constructor(name){
+    super(name)
+  }
+  drivers(){
+   return this.trips().map( (trip) => { return trip.driver() } )
+  }
+
+}
+class Trip{
+  constructor(driver, passenger){
+    this.id = store[this.constructor.name.toLowerCase() + "s"].push(this) - 1
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    this.driverObj = driver
+    this.passengerObj = passenger
+  }
+  driver(){
+    return this.driverObj
+  }
+  passenger(){
+    return this.passengerObj
+  }
+}
