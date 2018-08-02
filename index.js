43f938b7cda8f837bb91d9bf96ab40bdb0077cc2
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 19:37:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let store = { drivers: [] }
 class Driver{
     constructor(name){
       this.name = name
-      this.id = store.drivers.(this) - 1
+      this.id = store.drivers.push(this) - 1
     }
 }
